42eb6964552f9fce236d89e675607bdd89bac784
Charan-Prabu-05 <2500030264@kluniversity.in>
2025-12-03 08:54:20 +0530

    3.12.25

---
+++ task3.js
@@ -0,0 +1,19 @@
+functionconvertNumbers(){
+    const num = parseInt(document.getElementById("input1").value);
+    const format = document.getElementById("formatSelect").value;
+    const resultDiv = document.getElementById("result");
+
+    if(isNaN(num)){
+        resultDiv.innerText="please enter a valid name";
+        resultDiv.style.color="red";
+        return;
+    }
+     let result ="";
+     if (format=== "oct"){
+        result = num.toString(8);
+     } else if (format === "hex"){
+        result = num.toString(16).toUpperCase();
+     }
+     resultDiv.style.color="green"; 
+     resultDiv.innerText=`Converted Value:${result}`;
+}
